6e613fc3a13e6c8bad0ee071d359b567af0e0479
se98100 <se98100@gmail.com>
2020-04-03 17:55:59 +0200

    chip8 variables

--- Chip8.cs
+++ Chip8.cs
@@ -1,4 +1,18 @@
 
 public class Chip8 {
-    
+    Memory ram;
+    private byte[] v;
+    private ushort i;
+    private byte dt;
+    private byte st; 
+    private ushort pc;
+    private byte sp;
+    private ushort[] stack;
+
+    public Chip8()
+    {
+        ram = new Memory();
+        v = new byte[16];
+        stack = new ushort[16];
+    }
 }
