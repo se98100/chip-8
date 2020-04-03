2a7e1af601f0facefa661e56042029b88e029b21
se98100 <se98100@gmail.com>
2020-04-03 16:18:46 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chip8_emulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
